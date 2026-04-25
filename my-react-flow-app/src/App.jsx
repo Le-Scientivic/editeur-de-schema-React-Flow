3e92651761546008d79d1bd12061753cce0fd705
Le-Scientivic <107316919+Le-Scientivic@users.noreply.github.com>
2026-04-25 17:48:37 +0200

    utilisation de vite et suivi de la doc react flow  :  un schéma react flow à 2 noeuds et possibilité des les déplacer et relier

---
+++ my-react-flow-app/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState, useCallback } from 'react';
+import { ReactFlow, Background, Controls, applyEdgeChanges, applyNodeChanges, addEdge } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+ 
+//noeuds initiaux utilisés dans <ReactFlow nodes={initialNodes}>
+const initialNodes = [
+  {
+    id: 'n1',
+    position: { x: 0, y: 0 },
+    data: { label: 'Node 1' },
+    type: 'input',
+  },
+  {
+    id: 'n2',
+    position: { x: 100, y: 100 },
+    data: { label: 'Node 2' },
+  },
+];
+
+const initialEdges = [
+  {
+    id: 'n1-n2',
+    source: 'n1',
+    target: 'n2',
+    type: 'step',
+    label: 'connects with',
+  },
+];
+
+//crée un écran pour aficher le Flow
+export default function App() {
+  //initialise l'état des noeuds et traits (pour leur interactions)
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    [],
+  );
+  const onConnect = useCallback(
+    (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    [],
+  );
+
+  return (
+    <div style={{ height: '100%', width: '100%' }}>
+      <ReactFlow nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      > 
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
+}
+
